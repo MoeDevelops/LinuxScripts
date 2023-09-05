00a83e65acd0cc860b1fc5358c171a7316dcaa77
Moritz Niesen <niesenmoritz@gmail.com>
2023-09-05 19:25:22 +0200

    Fixed bug:  CRLF -> LF, Added brave

--- DebianInstallScript.sh
+++ DebianInstallScript.sh
@@ -2,4 +2,4 @@
 sudo apt install -y kde-plasma-desktop flatpak plasma-discover-backend-flatpak spectacle yakuake
 sudo apt remove -y zutty kwalletmanagersudo
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub -y kdenlive okular gwenview kcalc elisa kamoso
+flatpak install flathub -y kdenlive okular gwenview kcalc elisa kamoso brave
