5b5ba1a195a41373b21f43e6a7b68752dfc7f724
MoeDev <143090643+MoeDevelops@users.noreply.github.com>
2024-01-29 07:20:09 +0100

    Update installPackages.sh
    Remove Librewolf

--- Fedora/installPackages.sh
+++ Fedora/installPackages.sh
@@ -7,4 +7,4 @@ sudo dnf install -y neofetch yakuake kate
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # Install flatpaks from flathub
-flatpak install flathub -y com.github.tchx84.Flatseal com.discordapp.Discord com.usebottles.bottles io.gitlab.librewolf-community org.libreoffice.LibreOffice org.gimp.GIMP com.spotify.Client
+flatpak install flathub -y com.github.tchx84.Flatseal com.discordapp.Discord com.usebottles.bottles org.libreoffice.LibreOffice org.gimp.GIMP com.spotify.Client
